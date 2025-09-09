ba510cd5f390f958c5617df5dac5d4077d894da5
ronyz1 <rzamir@cisco.com>
2025-09-09 15:07:44 +0300

    CXPL-7109: add cancel deployment command to cli

--- src/cli/cancel/cancel.py
+++ src/cli/cancel/cancel.py
@@ -18,7 +18,7 @@ def cancel(
     typer.secho(f"Try to Cancel the deployment for: {deployment_id}", fg=typer.colors.BRIGHT_BLUE)
     api = APIClient(base_url=get_deployment_base_url(env), env=env)
     response = api.post(
-        f"/cancel/", deployment_id=deployment_id, headers={"Content-Type": "application/json"}
+        f"/cancel/{deployment_id}", headers={"Content-Type": "application/json"}
     )
     if response.status_code != 200:
         typer.secho(
