d7c47893c7046754e97aedfe20395f0b2bd39286
ronyz1 <rzamir@cisco.com>
2025-09-09 13:22:28 +0300

    CXPL-7109: add cancel deployment command to cli

--- src/cli/cancel/cancel.py
+++ src/cli/cancel/cancel.py
@@ -1,8 +1,7 @@
 import typer
 from cli.helpers.api_client import APIClient
 from cli.helpers.file import load_config
-from cli.config import get_deployment_base_url, ENVIRONMENTS
-
+from cli.config import get_deployment_base_url
 
 def cancel(
     deployment_id: str = typer.Argument(
@@ -11,12 +10,8 @@ def cancel(
         show_default=False,
         case_sensitive=False,
     ),
-        env: str = typer.Argument(
-            ...,
-            help=f"Environment (one of: {', '.join(ENVIRONMENTS)})",
-            show_default=False,
-            case_sensitive=False,
-        )):
+        env: str = typer.Argument("dev")
+):
     """
     Terminate the running deployment when it is in the validation phase.
     """
